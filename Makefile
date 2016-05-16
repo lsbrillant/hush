bd9a0041c667facd699b2d6e8bab41b6cbd74ded
Lucien Brillant <lucienbrillant@gmail.com>
2016-05-15 18:57:01 -0700

    start using the gnu readline library

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
-hush: main.o makeargv.o command.o env.o opts.o
-	cc -o hush build/main.o build/makeargv.o build/command.o build/env.o build/opts.o
+OBJ = build/main.o build/makeargv.o \
+	  build/command.o build/env.o \
+	  build/opts.o build/hsignal.o
+
+LIBDIR = /lib
+LIB = $(LIBDIR)/x86_64-linux-gnu/libreadline.so.6
+
+hush: main.o makeargv.o command.o env.o opts.o hsignal.o
+	cc -o hush $(OBJ) $(LIB)
 
 main.o: src/main.c  include/argv.h
 	cc -c src/main.c -pthread -I include -o build/main.o
@@ -16,5 +23,8 @@ command.o: src/command.c include/command.h  include/env.h include/argv.h
 opts.o: src/opts.c include/opts.h
 	cc -c src/opts.c -I include -pthread -o build/opts.o
 
+hsignal.o: src/signalsetup.c include/hushsignal.h
+	cc -c src/signalsetup.c -I include -o build/hsignal.o
+
 clean:
-	rm build/main.o build/makeargv.o build/env.o build/command.o hush
+	rm build/main.o build/makeargv.o build/env.o build/command.o build/hsignal.o hush
--- src/main.c
+++ src/main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include "argv.h"
 #include "command.h"
 #include "env.h"
 #include "opts.h"
+#include "hushsignal.h"
 
 #define HUSH_VERSION 1
 
@@ -14,15 +17,14 @@
 
 int main ( int argc, char ** argv ) 
 {	
-	char delim[] = " \t\n";
+    sigset_t blockmask;
+    struct sigaction defaction;
 	char * line;
 	size_t len = 0;
 	pid_t pid;
     int status;
 	FILE *readfrom = stdin;
-    
-	import_env(); 
-    
+     
     do_opts( argc, argv );
      
     if( forced_interactive ||
@@ -32,6 +34,15 @@ int main ( int argc, char ** argv )
             isatty(fileno(stdout))
         )
     ) interactive = 1;
+
+    if ( signal_setup(&defaction, &blockmask, SIG_IGN) == -1 ) {
+        perror("Failed to set up shell signal handling");
+        exit(EXIT_FAILURE);
+    }
+    if ( sigprocmask(SIG_BLOCK, &blockmask, NULL) == -1 ) {
+        perror("Failed to block signals");
+        exit(EXIT_FAILURE);
+    }
 
     /* running a file in the form hush FILE */
     if( optind < argc ) {
@@ -43,11 +54,13 @@ int main ( int argc, char ** argv )
     }
     
 	while (1) {
-        if ( interactive ) 
-            printf( "hushv%d> ",HUSH_VERSION );
-		if ( getline( &line, &len, readfrom ) == -1 ) {
-			if ( interactive )
+        if ( ! interactive ) { 
+            if ( getline( &line, &len, readfrom ) == -1 ) {
                 printf("\n");
+                break;
+            }
+        } else if ( ( line = readline("hush> ") ) == NULL ) {
+            printf("\n");
             break;
         }
         if ( strcmp(line, QUIT_STRING) == 0 ) {
@@ -56,7 +69,13 @@ int main ( int argc, char ** argv )
             continue;
         }
         if (wipe_comments(line) != 1) {
-            if ( (pid = fork()) == 0 ){
+            if ( (pid = fork()) == 0 ) {
+                if ((sigaction(SIGINT,&defaction,NULL) == -1) ||
+                    (sigaction(SIGQUIT,&defaction,NULL) == -1) ||
+                    (sigprocmask(SIG_UNBLOCK,&blockmask,NULL) == -1)) {
+                    perror("Failed to set signal handling for command");
+                    exit(EXIT_FAILURE);
+                }
                 run_command( line );
             }
             waitpid(pid, &status, WUNTRACED | WCONTINUED);
